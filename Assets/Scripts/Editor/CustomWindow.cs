f22d189a7855035e58ad18eb8d36f7992b77107e
Catherine <Catherine.Semiletova@gmail.com>
2022-07-26 09:34:45 +0200

    Search window

--- Assets/Scripts/Editor/CustomWindow.cs
+++ Assets/Scripts/Editor/CustomWindow.cs
@@ -1,15 +1,30 @@
+using System;
 using UnityEditor;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 namespace Editor
 {
     public class CustomWindow : EditorWindow
     {
+        private SearchField _searchField;
+        private String _findRequest;
+        
         [MenuItem("Tools/CustomEditor")]
         public static void OpenWindow()
         {
             EditorWindow windowToOpen = GetWindow<CustomWindow>();
             var content = new GUIContent("Custom window");
+        }
+
+        public void CreateGUI()
+        {
+            _searchField = new SearchField();
+        }
+
+        private void OnGUI()
+        {
+            var newFindRequest = _searchField?.OnToolbarGUI(_findRequest);
         }
     }
 }

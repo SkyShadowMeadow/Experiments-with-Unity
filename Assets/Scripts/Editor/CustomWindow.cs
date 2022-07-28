eb3600b84b42b868b5f1b8352f7f18388c1bc41e
Catherine <Catherine.Semiletova@gmail.com>
2022-07-28 10:26:54 +0200

    Custom window is finished

--- Assets/Scripts/Editor/CustomWindow.cs
+++ Assets/Scripts/Editor/CustomWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Configuration;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
@@ -12,6 +13,9 @@ namespace Editor
         private List<ObjectConfiguration> _configs;
         private SearchField _searchField;
         private String _findRequest;
+        private string[] _configsNames;
+        private bool[] _isFoldout;
+        private Vector2 _scrollPos;
         
         [MenuItem("Tools/CustomEditor")]
         public static void OpenWindow()
@@ -24,11 +28,70 @@ namespace Editor
         {
             _searchField = new SearchField();
             _configs = ObjectConfiguration.GetAllConfigs();
+            _isFoldout = new bool[_configs.Count];
+            
+            _configsNames = new string[_configs.Count];
+            
+            for (var i = 0; i < _configs.Count; i++)
+            {
+                _configsNames[i] = GetConfigName(_configs[i]);
+            }
         }
 
         private void OnGUI()
         {
             var newFindRequest = _searchField?.OnToolbarGUI(_findRequest);
+            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, false, true);
+            
+            for (var i = 0; i < _configs.Count; i++)
+            {
+                var objectConfig = _configs[i];
+                var serializedObject = new SerializedObject(objectConfig);
+
+                _isFoldout[i] = EditorGUILayout.Foldout(_isFoldout[i], GetConfigName(objectConfig));
+
+                if (_isFoldout[i])
+                    DrawSerializedObject(serializedObject);
+            }
+            
+            EditorGUILayout.EndScrollView();
+        }
+        
+        
+        private string GetConfigName(ObjectConfiguration config)
+        {
+            return config.GetType().Name;
+        }
+        
+        private void DrawSerializedObject(SerializedObject serializedObject)
+        {
+            if (serializedObject == null)
+            {
+                EditorGUILayout.HelpBox("Target SerializedObject is null!", MessageType.Warning);
+                return;
+            }
+
+            // display serializedProperty with selected mode
+            Type type = typeof(SerializedObject);
+            PropertyInfo infor = type.GetProperty("inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (infor != null)
+            {
+                infor.SetValue(serializedObject, InspectorMode.Normal, null);
+            }
+
+            var iterator = serializedObject.GetIterator();
+
+            //first call draw target script
+            iterator.NextVisible(true);
+
+
+            while (iterator.NextVisible(false))
+            {
+                //  enterChildren = false;
+                EditorGUILayout.PropertyField(iterator, true);
+            }
+
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
